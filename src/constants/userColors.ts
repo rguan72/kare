5288ac96f55dcbf7dd58ec1c6dd1825a83234501
Richard Guan <rguan72@gmail.com>
2020-04-30 12:20:45 -0400

    Fix some minor userColors stuff

--- src/constants/userColors.ts
+++ src/constants/userColors.ts
@@ -1,10 +1,11 @@
 let colors = {
-  biege: "#FFECDE",
+  beige: "#FFECDE",
   red: "#E8CAD2",
   purple: "#F3EAFF",
   darkPurple: "#5505BA",
   blue: "#CAD9E8",
-  green: "C4FFE4",
+  green: "#C4FFE4",
+  yellow: "#FF9D76",
 };
 
 export default colors;
