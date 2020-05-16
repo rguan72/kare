004c65aef0974f1a8f96a9a2e8b9ef105183183a
liandrew <liandrew@umich.edu>
2020-05-15 23:54:58 -0400

    initial setup

---
+++ src/screens/Report.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import {
+	KeyboardAvoidingView,
+	View,
+	Text,
+	Keyboard,
+	TouchableOpacity,
+	ActivityIndicator,
+  } from "react-native";
+
+  export default function ReportPopup({ navigation }) {
+	  
+  };
+
+  
