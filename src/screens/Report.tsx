fa1ef49dd7eb7579bb55128c1eb9d55b46c65b77
liandrew <liandrew@umich.edu>
2020-05-16 18:39:59 -0400

    work from 05/15/2020, still wip

--- src/screens/Report.tsx
+++ src/screens/Report.tsx
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 import {
+	Animated,
 	KeyboardAvoidingView,
 	View,
 	Text,
 	Keyboard,
 	TouchableOpacity,
 	ActivityIndicator,
+	FlatList,
   } from "react-native";
+  
 
   export default function ReportPopup({ navigation }) {
 	  
   };
 
-  
+  const SlideUpView = (props) => {
+	  const slideAnim = useRef(new Animated.Value(0)).current
+
+	  React.useEffect(() => {
+		  Animated.timing(
+			  slideAnim,
+			  {
+				  toValue: 1,
+				  duration: 100,
+			  }
+		  ).start();
+	  }, [])
+
+	  return
+  }
+  Animated.timing(this.state.xPosition, {
+	toValue: 100,
+	easing: Easing.back(),
+	duration: 100
+  }).start();
+
