794904c9a3c80e5397e24fbbd22ab1e9da725a78
Richard Guan <rguan72@gmail.com>
2020-05-21 17:37:44 -0400

    Add full self harm words

--- src/utils/moderation.ts
+++ src/utils/moderation.ts
@@ -1,4 +1,4 @@
-let selfHarmre = /suicide|kill/gim;
+let selfHarmre = /murder|gun|psycho|suicide|hate|kill|attack|own life|live anymore|life isn't worth it|life is not worth it|shoot|rifle|handgun|shotgun|beat the shit/gim;
 
 function flagSelfHarm(message: string) {
   const arr = message.match(selfHarmre);
